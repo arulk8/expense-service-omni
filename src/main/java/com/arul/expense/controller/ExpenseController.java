bfca3faecb95cc97a04b139946dbea013c8143a3
arulk8 <arulmurugan194@gmail.com>
2024-12-23 18:22:44 +0530

    [Feat] Created a sample controller class

--- src/main/java/com/arul/expense/controller/ExpenseController.java
+++ src/main/java/com/arul/expense/controller/ExpenseController.java
@@ -1,4 +1,13 @@
 package com.arul.expense.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ExpenseController {
+
+    @GetMapping("/expenses")
+    public String getExpense(){
+        return "Getting the response from rest";
+    }
 }
